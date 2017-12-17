16e1e083a9b25055807b4a605d51e479f030e556
Martin Gustavo Kluck <martinkluck05@gmail.com>
2017-12-16 21:13:10 -0300

    Video 8

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class, 29)->create();
+
+        App\User::create([
+            'name' => 'Martin Gustavo Kluck',
+            'email' => 'martinkluck05@gmail.com',
+            'password' => bcrypt('mgkluck05')
+        ]);
+    }
+}
