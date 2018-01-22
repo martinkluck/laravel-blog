828e76d695364a68f23577ac84832e1a78bcec2f
Martin Gustavo Kluck <martinkluck05@gmail.com>
2018-01-22 20:50:33 -0300

    Video 17

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Post;
 use Illuminate\Http\Request;
+use App\Http\Requests\PostStoreRequest;
+use App\Http\Requests\PostUpdateRequest;
 use App\Http\Controllers\Controller;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +22,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::orderBy('id','DESC')->paginate();
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
@@ -24,7 +33,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.posts.create');
     }
 
     /**
@@ -33,9 +42,11 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostStoreRequest $request)
     {
-        //
+        $post = Post::create($request->all());
+        return redirect()->route('posts.edit', $post->id)
+            ->with('info', 'Entrada creada con éxito.');
     }
 
     /**
@@ -46,7 +57,8 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+        return view('admin.posts.show', compact('post'));
     }
 
     /**
@@ -57,7 +69,8 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('admin.posts.edit', compact('post'));
     }
 
     /**
@@ -67,9 +80,12 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(PostUpdateRequest $request, $id)
     {
-        //
+        $post = Post::find($id);
+        $post->fill($request->all())->save();
+        return redirect()->route('posts.edit', $post->id)
+            ->with('info', 'Entrada actualizada con éxito.');
     }
 
     /**
@@ -80,6 +96,7 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Post::find($id)->delete();
+        return back()->with('info','Eliminado correctamente');
     }
 }
